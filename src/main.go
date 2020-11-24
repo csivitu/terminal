f5053cfd1d6a45c68681e2bbeeaab2d71d70b1ea
Shiv10 <shivanshsharma2012@gmail.com>
2020-11-24 18:40:40 +0000

    feat: added uds server with flags

--- src/main.go
+++ src/main.go
@@ -2,11 +2,14 @@ package main
 
 import (
         "fmt"
-        "flag"
+		"flag"
+		"os"
+		"net"
+		"log"
+		"strings"
 )
 
 func main()  {
-        fmt.Println("Hello world!")
 		name := flag.String("n","", "Name of the session you want to create")
 		join := flag.String("j","", "Name of session you want to join")
         help := flag.Bool("h", false, "This is the help command")
@@ -29,6 +32,75 @@ func main()  {
 		} 
 
 		if *name != "" {
-                fmt.Println("Making new session with name: ", *name)
+				fmt.Println("Making new session with name: ", *name)
+				createServer(*name)
         }
+}
+
+func createServer(name string) {
+
+	addr := "/tmp/"+name+".sock"
+	if fileExists(addr){
+		fmt.Println("Session with name"+name+"already active.")
+		return
+	}
+
+	l, err := net.Listen("unix",addr)
+
+	if err!=nil {
+		log.Fatal("Error occurred :",err)
+	}
+
+	defer l.Close()
+
+	for {
+		conn, err := l.Accept()
+
+		if err!= nil {
+			log.Fatal("Error occurred :",err)
+		}
+
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(c net.Conn) {
+
+	for {
+
+		buf :=make([]byte, 1024)
+		n, err := c.Read(buf)
+		
+		if err!=nil {
+			log.Fatal("Error occurred while recieving data: ", err)
+		}
+
+		data := string(buf[0:n])
+		data = strings.TrimSpace(data)
+
+		fmt.Println(data)
+
+		var wr string 
+		fmt.Scanln(&wr)
+		wr = wr+"\n"
+		b := []byte(wr)
+
+		_, e := c.Write(b)
+
+		if e!=nil {
+			log.Fatal("Write error: ",err)
+		}
+
+	}
+	
+	
+}
+
+
+func fileExists(filename string) bool {
+    info, err := os.Stat(filename)
+    if os.IsNotExist(err) {
+        return false
+    }
+    return !info.IsDir()
 }
